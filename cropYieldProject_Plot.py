f38d81449f0873011e1777a93e4a0c9b044db6ff
camfry17 <cameron.fry2145@gmail.com>
2023-11-19 13:18:11 -0600

    Remove null values from plot

--- cropYieldProject_Plot.py
+++ cropYieldProject_Plot.py
@@ -5,6 +5,8 @@ def buildPlot(worldData, country, crop):
     #Initialize plot parameters, figure, and axes
     plt.rcParams['toolbar'] = 'None'
     fig,axs = plt.subplots(2, layout = 'constrained')
+    fig.set_figheight(6)
+    fig.set_figwidth(10)
 
     if (crop.lower() == 'all' and country.lower() == 'all'):
         figTitle = ('Crop Yield Average vs. Hunger Index Worldwide')
@@ -15,39 +17,40 @@ def buildPlot(worldData, country, crop):
     else:
         figTitle = (crop.capitalize() + " Yield vs. Hunger Index in " + country)
 
-    #Pull data into local variables
-    cYear = worldData.Year
-    ghYear = worldData.Year
-    unYear = worldData.Year
-    wsYear = worldData.Year
-    stYear = worldData.Year
-    cVal = worldData.Value
-    ghVal = worldData.Global_Hunger_Index_2021
-    unVal = worldData.Prevalence_of_underweight_weight_for_age_percent_of_children_under_5
-    wsVal = worldData.Prevalence_of_wasting_weight_for_height_percent_of_children_under_5
-    stVal = worldData.Prevalence_of_stunting_height_for_age_percent_of_children_under_5
+    #Pull data into local variables and ignore datapoints with no values
+    cYear = worldData.Year[np.isfinite(worldData.Value)]
+    ghYear = worldData.Year[np.isfinite(worldData.Global_Hunger_Index_2021)]
+    unYear = worldData.Year[np.isfinite(worldData.Prevalence_of_underweight_weight_for_age_percent_of_children_under_5)]
+    wsYear = worldData.Year[np.isfinite(worldData.Prevalence_of_wasting_weight_for_height_percent_of_children_under_5)]
+    stYear = worldData.Year[np.isfinite(worldData.Prevalence_of_stunting_height_for_age_percent_of_children_under_5)]
+    cVal = worldData.Value[np.isfinite(worldData.Value)]
+    ghVal = worldData.Global_Hunger_Index_2021[np.isfinite(worldData.Global_Hunger_Index_2021)]
+    unVal = worldData.Prevalence_of_underweight_weight_for_age_percent_of_children_under_5[np.isfinite(worldData.Prevalence_of_underweight_weight_for_age_percent_of_children_under_5)]
+    wsVal = worldData.Prevalence_of_wasting_weight_for_height_percent_of_children_under_5[np.isfinite(worldData.Prevalence_of_wasting_weight_for_height_percent_of_children_under_5)]
+    stVal = worldData.Prevalence_of_stunting_height_for_age_percent_of_children_under_5[np.isfinite(worldData.Prevalence_of_stunting_height_for_age_percent_of_children_under_5)]
 
     #Set up plots for crop yield graph
-    axs[0].scatter(cYear, cVal, color = '#6666ff')
-    axs[0].plot(cYear, cVal, color = '#6666ff')
-
+    axs[0].grid(True, color = '#eeeeee')
+    axs[0].scatter(cYear, cVal, color = '#6666ff', zorder = 2.5)
+    axs[0].plot(cYear, cVal, color = '#6666ff', zorder = 2.5)
     axs[0].set_ylabel("Amount in Metric Tonnes")
     axs[0].margins(0.1, 0.1)
 
     #Set up plots for global hunger graph
-    axs[1].scatter(ghYear, ghVal, color = '#0033ff', label = 'Hunger Index')
-    axs[1].scatter(unYear, unVal, color = '#fca000', label = 'Children underweight')
-    axs[1].scatter(wsYear, wsVal, color = '#02d63e', label = 'Wasting weight for height')
-    axs[1].scatter(stYear, stVal, color = '#de0404', label = 'Stunting height for age')
-    axs[1].plot(ghYear, ghVal, color = '#0033ff')
-    axs[1].plot(unYear, unVal, color = '#fca000')
-    axs[1].plot(wsYear, wsVal, color = '#02d63e')
-    axs[1].plot(stYear, stVal, color = '#de0404')
-
-    legend = axs[1].legend(loc = 'best', shadow = False, fontsize = 9, facecolor = '#bfc3c0')
-    legend.get_frame()
+    axs[1].grid(True, color = '#eeeeee')
+    axs[1].scatter(ghYear, ghVal, color = '#0033ff', label = 'Hunger Index', zorder = 2.5)
+    axs[1].scatter(unYear, unVal, color = '#fca000', label = 'Children underweight', zorder = 2.5)
+    axs[1].scatter(wsYear, wsVal, color = '#02d63e', label = 'Wasting weight for height', zorder = 2.5)
+    axs[1].scatter(stYear, stVal, color = '#de0404', label = 'Stunting height for age', zorder = 2.5)
+    axs[1].plot(ghYear, ghVal, color = '#0033ff', zorder = 2.5)
+    axs[1].plot(unYear, unVal, color = '#fca000', zorder = 2.5)
+    axs[1].plot(wsYear, wsVal, color = '#02d63e', zorder = 2.5)
+    axs[1].plot(stYear, stVal, color = '#de0404', zorder = 2.5)
     axs[1].set_ylabel("Percentage of Population")
     axs[1].margins(0.1, 0.1)
+
+    legend = axs[1].legend(loc = 'best', shadow = False, fontsize = 9, facecolor = '#dddddd', edgecolor = '#dddddd')
+    legend.get_frame()
 
     #Set up Figure Attributes
     fig.suptitle(figTitle)
